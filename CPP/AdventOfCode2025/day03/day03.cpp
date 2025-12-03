7658b53772ad6ee0f04fbaa5e6b295c53efa8816
Vladyslav Tochanenko <itochanenko@gmail.com>
2025-12-03 20:14:39 +0200

    Update logic for Day3

--- CPP/AdventOfCode2025/day03/day03.cpp
+++ CPP/AdventOfCode2025/day03/day03.cpp
@@ -11,10 +11,22 @@
 
 using namespace std;
 
+long long* pow_table;
+
+void init_pow_table(int decimals) {
+    pow_table = new long long[decimals + 1];
+
+    pow_table[0] = 1;
+    for (int i = 1; i <= decimals; i++) {
+        pow_table[i] = pow_table[i - 1] * 10;
+    }
+}
+
 long long highest_between_12(const long long highest, const long long new_number, const int batteries) {
     long long current_highest = highest;
+    long long temp;
     for (int i = 0; i < batteries; i++) {
-        const long long temp = stoll(to_string(highest).erase(i, 1) + to_string(new_number));
+        temp = (highest / pow_table[batteries - i] * pow_table[batteries - i - 1] + highest % pow_table[batteries - i - 1]) * 10 + new_number;
         if (temp > current_highest) current_highest = temp;
     }
     return current_highest;
@@ -30,12 +42,17 @@ long long highest_joltage(const string& line, const int batteries) {
 
 int main() {
     string path = "../day03/input.txt";
+    // correct answer: 172740584266849
     vector<string> lines = readLines(path);
+
+    const int batteries = 12;
+
+    init_pow_table(batteries);
 
     long long total = 0L;
 
     for (const auto& line : lines) {
-        long long highest = highest_joltage(line, 12);
+        const long long highest = highest_joltage(line, batteries);
         total += highest;
     }
 
