06215b560ae31e70649969017c0cda64a86d5b3d
Vladyslav Tochanenko <itochanenko@gmail.com>
2025-12-04 07:39:53 +0200

    Implement Day4 part 2

--- CPP/AdventOfCode2025/day04/day04.cpp
+++ CPP/AdventOfCode2025/day04/day04.cpp
@@ -29,6 +29,32 @@ int count_rolls(string* map, int size) {
     return res;
 }
 
+int remove_until_finished(string* map, int size) {
+    int res = 0;
+    bool finished = false;
+    do {
+        finished = true;
+        for (int i = 1; i < size - 1; i++) {
+            for (int j = 1; j < size - 1; j++) {
+                if (map[i][j] != '@') continue;
+                int surrounding_rolls = 0;
+                for (int k = -1; k <= 1; k++) {
+                    for (int l = -1; l <= 1; l++) {
+                        if (k == 0 && l == 0) continue;
+                        if (map[i + k][j + l] == '@') surrounding_rolls++;
+                    }
+                }
+                if (surrounding_rolls < 4) {
+                    res++;
+                    map[i][j] = '.';
+                    finished = false;
+                }
+            }
+        }
+    } while (!finished);
+    return res;
+}
+
 int main() {
     string path = "../day04/input.txt";
     vector<string> lines = readLines(path);
@@ -45,7 +71,7 @@ int main() {
         map[i + 1] = "." + lines[i] + ".";
     }
 
-    cout << count_rolls(map, size) << endl;
+    cout << remove_until_finished(map, size) << endl;
 
     delete[] map;
     return 0;
